34082b3ecb1273acf448a695459828cc810e1cbe
mikepitiwat <52405022+mikepitiwat@users.noreply.github.com>
2020-01-22 11:44:32 +0700

    Update PreSJF.cpp

--- PreSJF.cpp
+++ PreSJF.cpp
@@ -9,6 +9,15 @@ typedef struct{
     int end;
 }Process;
 
+typedef struct{
+    int indexP;
+    int BurtT;
+}Queue;
+typedef struct{
+    int indexP;
+    int startP;
+}Gantt_C;
+
 //Process  burt time , Arrival time , Priority
 Process P[N+1]  =  {{0},
         /*P1*/ {    9     ,       1      ,      3},     // P1 = P[1]
@@ -17,29 +26,22 @@ Process P[N+1]  =  {{0},
         /*P4*/ {    4     ,       4      ,      4},     // P4 = P[4]
         /*P5*/ {    2     ,       7      ,      2}};    // P5 = P[5]
 
-int Gantt[20];
-int Q[99];       // คิว
+
+Gantt_C Gantt[20];
+Queue   Q[20];   // คิว
 int SP = 0;      // ชี้ค่าในคิว
 int NG = 0;      // number Gantt_chart
-int NT = 24;     // number time
+int NT = 30;     // number time
 int NP = N;      // number process
 
-void push(int index){
+void push(int index, int BTimeLeft){
     SP++;
-    Q[SP] = index;
+    Q[SP].indexP = index;
+    Q[SP].BurtT  = BTimeLeft;
 }
 void pop(){
     if(SP == 0) printf("POP Over !!!!\n");
     else        SP--;
-}
-
-int duplicate(int index){               //  เช็คว่ามีการเกิดของโปรเซสเวลาเดียวกันไหม
-    int count = 0;
-    for (int i = 1; i <= NP; i++)
-        if(P[index].ArrivalT == P[i].ArrivalT)
-            count++;
-    if (count > 1)  return 1;           // ถ้ามีการเกิดพร้อมกัน   return 1
-    else            return 0;           // ถ้าไม่มีการเกิดพร้อมมกัน return 0
 }
 
 int minBurt(int index){                 // หาค่าเวลาโปรเซสที่ใช้เวลาทำงานน้อยที่สุด ในกรณี ที่โปรเซสเกิดพร้อมกัน
@@ -57,37 +59,28 @@ int minBurt(int index){                 // หาค่าเวลาโปร�
     }
     for (int j = 0; j < n; j++) {      // นำ process ตัวที่เหลือไปต่อคิว
         if(temp[j] != index2){
-            push(temp[j]);
+            push(temp[j],P[temp[j]].BurtT);
         }
     }
     return index2;                     // return โปรเซสที่ใช้เวลาในการทำงานน้อยที่สุด
 }
 
-int status(int index){
-    if(duplicate(index) == 0){          // ถ้าไม่ซ้ำ แปลว่าไม่มี process เกิดในเวลาเดียวกัน
-        if(SP == 0){                    // ไม่มี process ในคิว
-            return index;
-        }else{                          // มี process ในคิว
-            push(index);
-            return 0;
-        }
-    }else{                              // ถ้าซ้ำ แปลว่ามี process เกิดเวลาเดียวกัน
-        if(SP == 0){                    // ไม่มี process ในคิว
-            return minBurt(index);
-        }else{                          // มี process ในคิว
-            for (int i = 1; i <= NP; i++)
-                if(P[index].ArrivalT == P[i].ArrivalT)
-                    push(i);
-            return 0;
-        }
-    }
+int duplicate(int index){
+    int count = 0;
+    for (int i = 1; i <= NP; i++)
+        if(P[index].ArrivalT == P[i].ArrivalT)
+            count++;
+    if (count > 1)
+        return minBurt(index);
+    else
+        return index;
 }
 
-int sort_Q(){
-    int temp;
+Queue sort_Q(){
+    Queue temp;
     for (int i = 1; i <=SP ; i++) {
         for (int j = i+1; j <=SP ; j++) {
-            if(P[Q[i]].BurtT < P[Q[j]].BurtT){
+            if(Q[i].BurtT < Q[j].BurtT){  // Q[i].BurtT < Q[i].BurtT
                 temp = Q[i];
                 Q[i] = Q[j];
                 Q[j] = temp;
@@ -97,50 +90,73 @@ int sort_Q(){
     return Q[SP];
 }
 
-void Non_PreSJF(){
-    int index, END_P = 0;
-    for (int i = 0; i <NT ; ++i) {                  // i แทน time(เวลา)
-        for (int j = 1; j <=NP ; ++j) {
-            if(i == P[j].ArrivalT) {                // ณ เวลาที่ i มี process[j] เกิดขึ้น
-                index = status(j);                  // ตรวจสถานะ ต่างๆ เช่น การเกิดพร้อมกัน หรือ การต่อคิว  return0 คือ โปรเซสไปต่อคิว ไม่มีโปรเซสไหนออกมา
-                if(i >= END_P){                     // แปลว่าถ้าไม่มีโปรเซสไหนอยู่ในการทำงาน
-                    if(index == 0 && SP != 0){      // ถ้าไม่มีโปรเซสไหนออกมา แต่ในคิวยังมีโปรเซสรออยู่
-                        index = sort_Q();
-                        pop();
-                    }
-                    END_P = i + P[index].BurtT;     // เวลาจบการทำงานของโปรเซส
-                    Gantt[NG]       = index;
-                    P[index].start  = i;
-                    P[index].end    = END_P;
+void PreSJF(){
+    int runningP, index, END_P = 0;
+    for (int i = 0; i <NT ; i++) {
+        for (int j = 1; j <=NP ; j++){
+            if(i == P[j].ArrivalT) {
+                index = duplicate(j);
+                if(i >= END_P){
+                    END_P = i + P[index].BurtT;// เวลาจบการทำงานของโปรเซส
+                    runningP = index;
+                    Gantt[NG].indexP = index;
+                    Gantt[NG].startP = i;
                     NG++;
                     break;
-                } else{                             //ถ้ามีโปรเซสทำงานอยู่
-                    if(index != 0){                 //แต่มีโปรเซสเกิดใหม่ต้องการทำงาน
-                        push(index);
+                }else{
+                    if(P[index].BurtT < (P[runningP].BurtT - i) ){
+                        push(runningP, (P[runningP].BurtT - i));
+                        END_P = i + P[index].BurtT;
+                        runningP = index;
+                        Gantt[NG].indexP = index;
+                        Gantt[NG].startP = i;
+                        NG++;
+                        break;
+                    } else{
+                        push(index, P[index].BurtT);
                     }
                 }
             }
         }
-        if(i >= END_P && SP != 0){                  // ถ้าไม่มีโปรเซสไหนทำงาน แต่ใน Q ยังมีโปรเซสรออยู่
+        if(i >= END_P && SP != 0){
+            Queue index;
             index = sort_Q();
             pop();
-            END_P = i + P[index].BurtT;
-            Gantt[NG]       = index;
-            P[index].start  = i;
-            P[index].end    = END_P;
+            END_P = i+index.BurtT;  // q[index].b
+            runningP = index.indexP;
+            Gantt[NG].indexP = index.indexP;
+            Gantt[NG].startP = i;
             NG++;
         }
     }
 }
 
+float waitProcess(int indexP){
+    int count = 0;
+    float waitT =0, end = 0;
+    for (int i = 0; i <NG ; i++) {
+        if(Gantt[i].indexP == indexP){
+            if(count == 0){
+                waitT += (float)Gantt[i].startP - P[indexP].ArrivalT;
+                end    = (float)Gantt[i+1].startP;
+                count++;
+            }else{
+                waitT += (float)Gantt[i].startP - end;
+                end    = (float)Gantt[i+1].startP;
+            }
+        }
+    }
+    return waitT;
+}
+
 int main(){
     printf("# Mr Pitiwat Promnimit ID:61543206020-5\n");
     printf("# OUTPUT LAB6 CPU Scheduling\n");
-    printf("# Non Preemptive SJF\n");
+    printf("# Preemptive SJF\n");
     printf("Sequence process is :");
-    Non_PreSJF();
+    PreSJF();
     for (int i = 0; i <NG ; i++) {
-        printf("P%d", Gantt[i]);
+        printf("P%d", Gantt[i].indexP);
         if(i<NG-1)
             printf("->");
     }
@@ -153,14 +169,14 @@ int main(){
     float sum=0;
     float avgTime;
     for (int i = 1; i <= NP; i++) {
-        printf("| %-11.2f",(float)P[i].start - P[i].ArrivalT);
-        sum += (float)P[i].start - P[i].ArrivalT;
+        printf("| %-11.2f",waitProcess(i));
+        sum += waitProcess(i);
     }
     avgTime = sum/NP;
     printf("\nAverage time is %.2f", avgTime);
     printf("\nTurnaround time\n");
     for (int i = 1; i <= NP; i++) {
-        printf("|P%d =%-6.2f ms ", i, ((float) P[i].start - P[i].ArrivalT) + P[i].BurtT);
+        printf("|P%d = %-6.2fms ", i, waitProcess(i) + P[i].BurtT);
     }
     printf("\n------------------------------------------------------------\n");
     return 0;
